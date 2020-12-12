4cd26b8a58609beff14d697a6fc7fe5032e2f841
sato-takehiro <sato.takehiro00@gmail.com>
2020-12-13 01:11:54 +0900

    ウェブフォントの適用とMENUの微調整完了、index.html完成

--- js/sidebar.js
+++ js/sidebar.js
@@ -4,32 +4,29 @@ $(function() {
     var $sidebar = $(".l-sidebar");
     var $sidebarOpponent = $(".l-sidebar__opponent");
     var $sidebarWrapper = $(".l-sidebar__wrapper");
-    var $sidebarTime = 500;
 
     $sidebarOpen.on('click', function() {//クリックした時、sidebarを表示、背景色を暗くする
 
         $sidebar.addClass("is_active");
-        $(this).addClass("is_active");
-        $sidebarClose.removeClass("is_active");
+        //$(this).addClass("is_active");
+        //$sidebarClose.removeClass("is_active");
         $sidebarOpponent.addClass("is_active");
         $sidebarWrapper.addClass("is_active");
-        $sidebar.show();
     });
 
     $sidebarClose.on('click', function() {//クリックした時、sidebarを非表示、背景色を戻す
         
         $sidebar.removeClass("is_active");
-        $(this).addClass("is_active");
-        $sidebarOpen.removeClass("is_active");
+        //$(this).addClass("is_active");
+        //$sidebarOpen.removeClass("is_active");
         $sidebarOpponent.removeClass("is_active");
         $sidebarWrapper.removeClass("is_active");
-        //$sidebar.hide($sidebarTime);
     });
 
     $(window).on('resize', function() {//画面横幅がPCサイズに変更したときsidebarをPC版の設定に戻す
             if(window.innerWidth > 959){//PC版の時
-            $sidebarOpen.addClass("is_active");
-            $sidebarClose.removeClass("is_active");
+            //$sidebarOpen.addClass("is_active");
+            //$sidebarClose.removeClass("is_active");
             $sidebarOpponent.removeClass("is_active");
             $sidebarWrapper.removeClass("is_active");
             $sidebar.removeClass("is_active");
