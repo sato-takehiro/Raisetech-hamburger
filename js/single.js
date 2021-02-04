b3279222455e5858d022cb2259789b30ce9dea67
sato-takehiro <sato.takehiro00@gmail.com>
2021-02-04 13:59:49 +0900

    投稿引用完成

---
+++ js/single.js
@@ -0,0 +1,17 @@
+$(function(){ //投稿ページであるsingle.phpで、投稿の見た目を整えるためにクラス名を付与する関数
+    
+    if ($('.post p').length) {//通常
+        $('.post p').addClass('u-mt312vw u-mt72vw-md u-mt8vw-sm u-mt0');
+    }
+    if ($('.post h1,.post h2,.post h3,.post h4,.post h5,.post h6').length) {//通常
+        $('.post h1,.post h2,.post h3,.post h4,.post h5,.post h6').addClass('u-mt312vw u-mt72vw-md u-mt8vw-sm');
+    }
+    if ($('.post .wp-block-quote').length) {//引用があるとき
+        $('.post .wp-block-quote').addClass('c-blockquote u-mt312vw u-mt72vw-md u-mt8vw-sm');
+        $('.post .wp-block-quote p').removeClass('u-mt312vw u-mt72vw-md u-mt8vw-sm u-mt0');
+        $('.post .wp-block-quote cite').addClass('c-blockquote__source');
+    }
+    if ($('.post .wp-block-image').length) {//画像があるとき
+        $('.post .wp-block-image').addClass('u-mt312vw u-mt72vw-md u-mt8vw-sm u-mt0');
+    }
+})
--- single.php
+++ single.php
@@ -1,48 +1,30 @@
 <?php get_header(); //header.phpを読み込むテンプレートタグ（インクルードタグ）?>
-            <section class="p-hero p-hero--single">
+            <!--<section class="p-hero p-hero--single">
                 <h1 class="c-ttl u-fs-40 u-fs-md-36">h1 ダミーサイト</h1>      
-            </section>
+            </section>-->
             
             <?php //記事を表示する
             while( have_posts() ) :
                 the_post(); ?>
-                <section <?php post_class('p-menu l-contents'); //自動でクラスを割り当てる?> id="post-<?php the_ID(); //投稿ID?>" <?php post_class(); //生成するページによってclassを付与する?>>
-                    <?php if( has_post_thumbnail() ) : ?>
-                        <?php the_post_thumbnail('thumbnail',array('class' => 'p-menu__img')) //設定されているときはアイキャッチ画像を表示?>
-                    <?php else: //サムネイルが設定されていなければデフォルト画像を表示する?>
-                        <picture class="p-menu__img">
-                            <source media="(min-width: 960px)" srcset="<?php echo get_template_directory_uri(); ?>/img/menu_img.png">
-                            <source media="(min-width: 560px)" srcset="<?php echo get_template_directory_uri(); ?>/img/menu_img-md.png">
-                            <img src="<?php echo get_template_directory_uri(); ?>/img/menu_img-sm.png">
-                        </picture>
-                    <?php endif; ?>
-                    <div class="p-menu__contents">
-                        <h3 class="p-menu__ttl"><?php the_title(); //投稿タイトルを表示?></h3>
-                            <?php $content_string = get_the_content('<a href=" the_permalink(); " class="c-button u-button">詳しく見る</a>');//投稿本文の一部のプレビュー、引数で「さらに...」を「詳しく見る」に置き換える 
-                            $content_string = str_replace('<p','<p class="p-menu__description" ',$content_string);
-                            $content_string = str_replace('<h1','<h1 class="p-menu__description" ',$content_string);
-                            $content_string = str_replace('<h2','<h2 class="p-menu__description" ',$content_string);
-                            $content_string = str_replace('<h3','<h3 class="p-menu__description" ',$content_string);
-                            $content_string = str_replace('<h4','<h4 class="p-menu__description" ',$content_string);
-                            $content_string = str_replace('<h5','<h5 class="p-menu__description" ',$content_string);
-                            $content_string = str_replace('<h6','<h6 class="p-menu__description" ',$content_string);
-                            echo $content_string; //p,h1-h6タグにクラス名を付け、スタイルを適用する?>
+                <div <?php post_class(); //生成するページによってclassを付与する?> id="post-<?php the_ID(); //投稿ID?>">
+                    <section class="p-hero p-hero--single p-menu">
+                        <?php if( has_post_thumbnail() ) : ?>
+                            <?php the_post_thumbnail('thumbnail',array('class' => 'p-menu__img'));//設定されているときはアイキャッチ画像を表示 ?>
+                        <?php else : //サムネイルが設定されていなければデフォルト画像を表示する?>
+                            <picture class="p-menu__img">
+                                <source media="(min-width: 960px)" srcset="<?php echo get_template_directory_uri(); ?>/img/menu_img.png">
+                                <source media="(min-width: 560px)" srcset="<?php echo get_template_directory_uri(); ?>/img/menu_img-md.png">
+                                <img src="<?php echo get_template_directory_uri(); ?>/img/menu_img-sm.png">
+                            </picture>
+                        <?php endif; ?>
+                        <h1 class="p-menu__ttl p-menu__ttl--single"><?php the_title(); //投稿タイトルを表示?></h1>
+                    </section>
+                    <div class="l-contents">    
+                        <?php the_content();//投稿本文を表示する?>
                     </div>
-                </section>
+                </div>
             <?php endwhile; ?>
             
-            <h2 class="l-contents u-mt312vw u-mt72vw-md u-mt8vw-sm">見出しh2</h2>
-            <p class="l-contents u-mt312vw u-mt72vw-md u-mt8vw-sm">Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。Pタグテキスト。</p>
-            <h3 class="l-contents u-mt312vw u-mt72vw-md u-mt8vw-sm">見出しh3</h3>
-            <h4 class="l-contents u-mt312vw u-mt72vw-md u-mt8vw-sm">見出しh4</h4>
-            <h5 class="l-contents u-mt312vw u-mt72vw-md u-mt8vw-sm">見出しh5</h5>
-            <h4 class="l-contents u-mt312vw u-mt72vw-md u-mt8vw-sm">見出しh4</h4>
-            <h5 class="l-contents u-mt312vw u-mt72vw-md u-mt8vw-sm">見出しh5</h5>
-            <h6 class="l-contents u-mt312vw u-mt72vw-md u-mt8vw-sm">見出しh6</h6>
-            <blockquote class="l-contents c-blockquote u-mt312vw u-mt72vw-md u-mt8vw-sm" cite="#">
-                <p class="c-blockquote__description">Blockquote 引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ</p>
-                <p class="c-blockquote__source">出典元： <a href="#">○○○○○○○○○○○○</a></p>
-            </blockquote>
             <section class="l-contents p-media p-media--only">
                 <img src="<?php echo get_template_directory_uri(); ?>/img/cooked-foods.png" class="p-media__img">
             </section>
